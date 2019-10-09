6425c765cb973ca4489b6fec46886d49b7c605b8
Nichkov Roman <44260533+Unleashed97@users.noreply.github.com>
2019-10-09 16:21:35 +0300

    vol. 1 start pack

---
+++ js/filter-works.js
@@ -0,0 +1,43 @@
+// let welcome = function()
+// {
+//   alert('welcome to my website');
+// }
+// welcome();
+
+
+//возвр все елементы
+  // let app = document.querySelectorAll('[data-filter]');
+  //
+  // for(let i=0; i<app.length; i++)
+  // {
+  //   app[i].addEventListener('click', funcClick);
+  //
+  // }
+  // function funcClick(event)
+  // {
+  //   // event.preventDefault();
+  //   for(let i=0; i<app.length; i++)
+  //   {
+  //     console.log(app[i].getAttribute('data-filter'));
+  //   }
+  // }
+  // funcClick();
+
+  // let app = document.querySelectorAll("[data-filter='app']");
+
+
+// возвр Node list
+  // let app = document.querySelectorAll('a[data-filter]');
+  //
+  // for(let i=0; i<app.length; i++)
+  // {
+  //   app[i].addEventListener('click', funcClick);
+  //
+  // }
+  // function funcClick(e)
+  // {
+  //   // e.preventDefault();
+  //   console.log(app);
+  //
+  // }
+  // funcClick();
