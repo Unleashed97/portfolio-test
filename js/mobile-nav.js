4c6e7b88a81bc41b90ef787262c0d0696f54050e
Nichkov Roman <nichkovroman@gmail.com>
2019-10-26 00:33:11 +0300

    adaptive design vol. 3
    hamburger icon animated(vol. 1),
    news section, date icons adapted for all devices

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,9 +1,20 @@
+// let btnMenu = document.getElementById("nav-toggle");
+// let nav = document.getElementById("nav");
+// btnMenu.onclick = function (event)
+// {
+//   event.preventDefault();
+//   nav.classList.toggle("show");
+//   if(nav.classList.contains('show')) document.body.classList.add("no-scroll");
+//   else document.body.classList.remove("no-scroll");
+// }
+
 let btnMenu = document.getElementById("nav-toggle");
 let nav = document.getElementById("nav");
 btnMenu.onclick = function (event)
 {
   event.preventDefault();
   nav.classList.toggle("show");
+  btnMenu.classList.toggle("open")
   if(nav.classList.contains('show')) document.body.classList.add("no-scroll");
   else document.body.classList.remove("no-scroll");
 }
